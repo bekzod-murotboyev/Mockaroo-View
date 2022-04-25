7ab27a2b627dae52bb5379b56aee4662ff3b73c1
bekdev01 <bekzod.m070@gmail.com>
2022-04-25 17:09:21 +0500

    ok

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -2,32 +2,33 @@ import axios from "axios";
 import {apiCall} from "../api";
 
 
-const api=({dispatch})=>(next)=>(action)=>{
-    if(action.type!==apiCall.type){
+const api = ({dispatch}) => (next) => (action) => {
+    if (action.type !== apiCall.type) {
         next(action);
         return;
     }
     next(action)
-    const {url,method,onSuccess,onFail,data}=action.payload
-    let token=localStorage.getItem('accessToken')
+    const {url, method, onSuccess, onFail, data} = action.payload
+    let token = localStorage.getItem('accessToken')
 
-    let headers=token?{'Authorization':'Bearer '+token}:''
+    let headers = token ? {'Authorization': 'Bearer ' + token} : ''
 
     axios({
-        baseURL:'http://reactive-mockaroo.us-east-1.elasticbeanstalk.com/api/',
+        // baseURL:'http://reactive-mockaroo.us-east-1.elasticbeanstalk.com/api/',
+        baseURL: 'http://localhost:8080/api/',
         url,
         method,
         data,
         headers
-    }).then(res=>{
+    }).then(res => {
         dispatch({
-            type:onSuccess,
-            payload:res.data
+            type: onSuccess,
+            payload: res.data
         })
-    }).catch(err=>{
+    }).catch(err => {
         dispatch({
-            type:onFail,
-            payload:err.data
+            type: onFail,
+            payload: err.data
         })
     })
 }
